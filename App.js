53b36d632412dccb0d95335d0c696f7fa1bb387a
martinbeer <120416183+martinbeer@users.noreply.github.com>
2024-11-17 17:24:38 +0100

    Einführung_React-Native

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen'; // Removed curly braces
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
