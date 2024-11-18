1ebe5bf7b46a12d5af0ce7b337bb23d672391ee9
Stefan Ilic <104595335+Stefan98167@users.noreply.github.com>
2024-11-18 11:42:31 +0100

    Added Montserrat font

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Montserrat': require("./assets/fonts/Montserrat-Regular.ttf"),
+    'Montserrat-SemiBold': require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    'Montserrat-Bold': require("./assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <HomeScreen />
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
